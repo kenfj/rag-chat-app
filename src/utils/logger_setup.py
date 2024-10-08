643c38980d24def453e214e41ece0a8833be4675
Kenfj <44661517+kenfj@users.noreply.github.com>
2024-10-08 18:08:11 +0900

    fix duplicate logging

--- src/utils/logger_setup.py
+++ src/utils/logger_setup.py
@@ -1,29 +1,49 @@
 import logging
+
 from uvicorn.logging import DefaultFormatter
+
+from utils import settings
 
 # https://zenn.dev/techflagcorp/articles/8d6327311e1e9f
 
-# uvicorn DefaultFormatter for consistent log format
+# uvicorn.logging.DefaultFormatter for consistent log format
 # https://stackoverflow.com/questions/62955750/what-does-do-in-python-log-config
 
-logging.getLogger("requests").setLevel(logging.WARNING)
+# suppress log messages from third party libraries
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("LiteLLM").setLevel(logging.WARNING)
+logging.getLogger("chainlit").setLevel(logging.WARNING)
+logging.getLogger("azure.core").setLevel(logging.WARNING)
+
+# for debug to find the logger name
+# logging.basicConfig(format="%(levelname)s %(asctime)s %(name)s %(message)s", datefmt="%Y-%m-%d %H:%M:%S", force=True)
+
+# https://stackoverflow.com/questions/7173033/duplicate-log-output-when-using-python-logging-module
+loggers = {}
 
 
-def setup_logger(name, level=logging.INFO):
+def setup_logger(name):
+    if name in loggers:
+        return loggers[name]
+
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    logger.setLevel(settings.LOG_LEVEL)
 
-    if not logger.hasHandlers():
-        handler = logging.StreamHandler()
+    # Note: logger.hasHandlers() will not work as expected
 
-        formatter = DefaultFormatter(
-            fmt="%(levelprefix)s %(asctime)s %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-            use_colors=True,
-        )
-        handler.setFormatter(formatter)
+    handler = logging.StreamHandler()
+    handler.setLevel(settings.LOG_LEVEL)
 
-        logger.addHandler(handler)
+    formatter = DefaultFormatter(
+        fmt="%(levelprefix)s %(asctime)s %(name)s %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        use_colors=True,
+    )
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.propagate = False
+
+    loggers[name] = logger
 
     return logger
--- src/utils/settings.py
+++ src/utils/settings.py
@@ -13,3 +13,6 @@ LLM_API_BASE = os.environ["LLM_API_BASE"]
 
 SEARCH_ENDPOINT = os.environ["SEARCH_ENDPOINT"]
 SEARCH_API_KEY = os.environ["SEARCH_API_KEY"]
+
+# logging settings
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
